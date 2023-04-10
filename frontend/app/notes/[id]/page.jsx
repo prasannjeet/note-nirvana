b3908876f7a58272ff42d0302d9a4cc102e3b5d5
Dennis Demir <demir.petrus@gmail.com>
2023-04-10 17:20:26 +0200

    Adding buttons to note page

--- frontend/app/notes/[id]/page.jsx
+++ frontend/app/notes/[id]/page.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { FaArrowLeft, FaEdit, FaTrash } from "react-icons/fa"
 
 const note = {
     id: 1,
@@ -7,19 +8,27 @@ const note = {
 }
 
 const NotePage = async () => {
-  return (
-    <div>
-      <section className="">
-        <Link href={"/notes"}>Go back</Link>
-        <div>
-            <h1>{note.title}</h1>
-        </div>
-        <div>
-            <p>{note.content}</p>
-        </div>
-      </section>
-    </div>
-  )
+    return (
+        <>
+            <div className="mx-4 my-4 flex justify-between">
+                <Link className="py-4 px-4 bg-gray-400 rounded-md text-white" href={"/notes"}>
+                    <FaArrowLeft />
+                </Link>
+                <div className="flex">
+                    <FaTrash className="mr-4" />
+                    <FaEdit />
+                </div>
+            </div>
+            <section className="flex flex-col justify-center content-center items-center">
+                <div>
+                    <h1>{note.title}</h1>
+                </div>
+                <div className="w-1/2">
+                    <p>{note.content}</p>
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default NotePage
