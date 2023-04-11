e8fb06a3928c4a4a595c92477bb79d6dfd6f03a2
Prasannjeet Singh <prasannjeet.singh@ist.com>
2023-04-11 14:20:31 +0200

    Keycloak Integration working with frontend

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,20 +1,36 @@
+'use client'
 import Navbar from './components/Navbar'
 import './globals.css'
+import {NextKeycloakAuthProvider} from "@krashnakant/next-keycloak";
 
-export const metadata = {
-  title: 'Nirvana Notes',
-  description: 'Your productivity note taking app.',
-}
+// export const metadata = {
+//     title: 'Nirvana Notes',
+//     description: 'Your productivity note taking app.',
+// }
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>
-        {children}
-        </main>
-      </body>
-    </html>
-  )
+const config = {
+    realm: process.env.NEXT_PUBLIC_KEYCLOAK_REAM,
+    url: process.env.NEXT_PUBLIC_KEYCLOAK_URL,
+    clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID,
+};
+
+const initOption = {
+    onLoad: 'check-sso',
+    silentCheckSsoRedirectUri:
+        'http://localhost:3000/silent-check-sso.html',
+};
+
+export default function RootLayout({children}) {
+    return (
+        <html lang="en">
+        <body>
+        <NextKeycloakAuthProvider config={config} initOption={initOption}>
+            <Navbar/>
+            <main>
+                {children}
+            </main>
+        </NextKeycloakAuthProvider>
+        </body>
+        </html>
+    )
 }
