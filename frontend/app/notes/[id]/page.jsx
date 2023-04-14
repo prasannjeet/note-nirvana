732e97c97aaed57e9df7bf6f59a296facae68214
Dennis Demir <demir.petrus@gmail.com>
2023-04-14 19:47:33 +0200

    Handling functionality to edit note

--- frontend/app/notes/[id]/page.jsx
+++ frontend/app/notes/[id]/page.jsx
@@ -9,6 +9,9 @@ import { useRouter } from 'next/navigation';
 
 const NotePage = ({ params: { id } }) => {
     const [note, setNote] = useState();
+    const [editForm, setEditForm] = useState(false);
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
 
     const { token, authenticated } = useNextKeycloakAuth();
 
@@ -54,6 +57,36 @@ const NotePage = ({ params: { id } }) => {
         router.push(`/notes`)
     }
 
+    const openEditForm = () => {
+        setEditForm(!editForm);
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const res = await fetch(`https://nirvana.ooguy.com/api/v1/notes/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                title: title,
+                content: content
+            })
+        })
+
+        const data = await res.json()
+        setNote(data)
+        console.log("Submitted DATA:",data);
+
+        if (data) {
+            router.push(`/notes/${id}`)
+            setEditForm(false)
+        }
+    }
+
 
 
     return (
@@ -65,8 +98,8 @@ const NotePage = ({ params: { id } }) => {
                             <FaArrowLeft />
                         </Link>
                         <div className="flex">
-                            <FaTrash className="mr-4" onClick={handleDelete} />
-                            <FaEdit />
+                            <FaTrash className="mr-4 cursor-pointer" onClick={handleDelete} />
+                            <FaEdit className='cursor-pointer' onClick={openEditForm} />
                         </div>
                     </div>
                     <section className="flex flex-col justify-center content-center items-center">
@@ -76,6 +109,20 @@ const NotePage = ({ params: { id } }) => {
                         <div className="w-1/2">
                             <p>{note.content}</p>
                         </div>
+                        {/* Open edit form */}
+                        {editForm && (
+                            <>
+                                <form onSubmit={handleSubmit} className="flex flex-col py-4 px-4 bg-gray-200 w-1/2">
+                                <label htmlFor="title">Title</label>
+                                <input className="h-10 px-2 rounded-md" type="text" name="title" id="title" onChange={(e) => setTitle(e.target.value)} />
+                                <label htmlFor="content">Content</label>
+                                <textarea className="rounded-md h-12 px-2 py-2" name="content" id="content" onChange={(e) => setContent(e.target.value)} />
+                                <div className="py-4 mx-auto">
+                                    <button type='submit' className="bg-green-600 text-white rounded-md py-2 px-2 hover:bg-green-700">Update note</button>
+                                </div>
+                                </form>
+                            </>
+                        )}
                     </section>
                 </>
             ) : (
