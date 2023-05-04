e7e0e5ead4d3eead0e1b9c7244250284b7ec9011
Dennis Demir <demir.petrus@gmail.com>
2023-05-04 20:17:35 +0200

    release 2.0.1

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-    <h1>Welcome to Nirvana Notes</h1>
+    <h1>Welcome to Nirvana Notes - Your note companion</h1>
     </>
   )
 }
