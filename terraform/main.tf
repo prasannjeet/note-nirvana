ac9b75f9e58d82d8e898bbc69129456e88e70f06
Prasannjeet Singh <ps222vt@student.lnu.se>
2023-05-04 17:31:15 +0200

    added terraform and keycloak configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+variable "user_name" {}
+variable "tenant_name" {}
+variable "tenant_id" {}
+variable "user_domain_name" {}
+variable "password" {}
+variable "auth_url" {}
+variable "region" {}
+variable "user_keyPiar" {}
+
 # Define required providers
 terraform {
 required_version = ">= 0.14.0"
@@ -9,21 +18,81 @@ required_version = ">= 0.14.0"
   }
 }
 
+# Configure the OpenStack Provider
 provider "openstack" {
-  auth_url     = var.OS_AUTH_URL
-  user_name    = var.OS_USERNAME
-  password     = var.OS_PASSWORD
-  tenant_name  = var.OS_PROJECT_NAME
+  user_name   = "${var.user_name}"
+  tenant_name = "${var.tenant_name}"
+  tenant_id   = "${var.tenant_id}"
+  user_domain_name = "${var.user_domain_name}"
+  password    = "${var.password}"
+  auth_url    = "${var.auth_url}"
+  region      = "${var.region}"
 }
 
-resource "openstack_networking_network_v2" "network_1" {
-  name           = "network_1"
+
+# Create a security group for SSH access
+resource "openstack_compute_secgroup_v2" "ssh" {
+  name        = "ssh"
+  description = "ssh"
+
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+}
+
+
+# Create a security group for http
+resource "openstack_compute_secgroup_v2" "http" {
+    name        = "http"
+    description = "htp"
+
+    rule {
+        from_port   = 81
+        to_port     = 81
+        ip_protocol = "tcp"
+        cidr        = "0.0.0.0/0"
+    }
+  
+}
+
+# Create a security group for https
+resource "openstack_compute_secgroup_v2" "https" {
+    name        = "https"
+    description = "htps"
+
+    rule {
+        from_port   = 443
+        to_port     = 443
+        ip_protocol = "tcp"
+        cidr        = "0.0.0.0/0"
+    }
+}
+
+# Create a security group for tcp
+resource "openstack_compute_secgroup_v2" "tcp" {
+    name        = "tcp"
+    description = "tcp"
+
+   rule {
+    from_port   = 80
+    to_port     = 80
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+}
+
+resource "openstack_networking_network_v2" "network" {
+  name           = "network"
   admin_state_up = "true"
 }
 
-resource "openstack_networking_subnet_v2" "subnet_1" {
-  name       = "subnet_1"
-  network_id = openstack_networking_network_v2.network_1.id
+resource "openstack_networking_subnet_v2" "subnet" {
+  name       = "subnet"
+  network_id = openstack_networking_network_v2.network.id
   cidr       = "192.168.0.0/24"
   ip_version = 4
 
@@ -37,300 +106,99 @@ resource "openstack_networking_subnet_v2" "subnet_1" {
   dns_nameservers = ["8.8.8.8", "1.1.1.1"]
 }
 
-resource "openstack_networking_router_v2" "router_1" {
-  name                = "router_1"
+resource "openstack_networking_router_v2" "router" {
+  name                = "router"
   admin_state_up      = "true"
-  external_network_id = var.PUBLIC_NETWORK_UUID # Replace with the UUID of the "public" network
-}
-
-resource "openstack_networking_router_interface_v2" "router_gateway_1" {
-  router_id = openstack_networking_router_v2.router_1.id
-  subnet_id = openstack_networking_subnet_v2.subnet_1.id
-}
-
-resource "openstack_compute_secgroup_v2" "secgroup_1" {
-  name        = "secgroup_1"
-  description = "Secgroup allowing required ports"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_1_http" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 80
-  port_range_max    = 80
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_compute_secgroup_v2.secgroup_1.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_1_https" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 443
-  port_range_max    = 443
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_compute_secgroup_v2.secgroup_1.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_1_ssh" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_compute_secgroup_v2.secgroup_1.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_1_tcp_10000" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 10000
-  port_range_max    = 10000
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_compute_secgroup_v2.secgroup_1.id
-}
-
-
-resource "openstack_compute_secgroup_v2" "secgroup_2" {
-  name        = "secgroup_2"
-  description = "Secgroup allowing required ports"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_2_http" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 80
-  port_range_max    = 80
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_compute_secgroup_v2.secgroup_2.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_2_https" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 443
-  port_range_max    = 443
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_compute_secgroup_v2.secgroup_2.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_2_ssh" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_compute_secgroup_v2.secgroup_2.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_2_tcp_10000" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 10000
-  port_range_max    = 10000
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_compute_secgroup_v2.secgroup_2.id
-}
-
-resource "openstack_compute_secgroup_v2" "secgroup_3" {
-  name        = "secgroup_3"
-  description = "Secgroup allowing required ports"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_3_http" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 80
-  port_range_max    = 80
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_compute_secgroup_v2.secgroup_3.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_3_https" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 443
-  port_range_max    = 443
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_compute_secgroup_v2.secgroup_3.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_3_ssh" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_compute_secgroup_v2.secgroup_3.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_3_tcp_10000" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 10000
-  port_range_max    = 10000
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_compute_secgroup_v2.secgroup_3.id
+  external_network_id = "fd401e50-9484-4883-9672-a2814089528c" # Replace with the UUID of the "public" network
 }
 
 resource "openstack_networking_port_v2" "port_1" {
   name           = "port_1"
-  network_id     = openstack_networking_network_v2.network_1.id
+  network_id     = openstack_networking_network_v2.network.id
   admin_state_up = "true"
 
   fixed_ip {
-    subnet_id = openstack_networking_subnet_v2.subnet_1.id
+    subnet_id = openstack_networking_subnet_v2.subnet.id
   }
 }
 
 resource "openstack_networking_port_v2" "port_2" {
   name           = "port_2"
-  network_id     = openstack_networking_network_v2.network_1.id
+  network_id     = openstack_networking_network_v2.network.id
   admin_state_up = "true"
 
   fixed_ip {
-    subnet_id = openstack_networking_subnet_v2.subnet_1.id
+    subnet_id = openstack_networking_subnet_v2.subnet.id
   }
 }
 
 resource "openstack_networking_port_v2" "port_3" {
   name           = "port_3"
-  network_id     = openstack_networking_network_v2.network_1.id
+  network_id     = openstack_networking_network_v2.network.id
   admin_state_up = "true"
 
   fixed_ip {
-    subnet_id = openstack_networking_subnet_v2.subnet_1.id
+    subnet_id = openstack_networking_subnet_v2.subnet.id
   }
 }
 
-resource "openstack_compute_instance_v2" "instance_1" {
-  name            = "instance_1"
-  security_groups = [openstack_compute_secgroup_v2.secgroup_1.name]
-  key_pair        = "ps222vt_Keypair"
+resource "openstack_networking_router_interface_v2" "router_gateway_1" {
+  router_id = openstack_networking_router_v2.router.id
+  subnet_id = openstack_networking_subnet_v2.subnet.id
+}
 
-  flavor_name = "c4-r8-d80"
-  image_id = "f73ec00b-6ea9-456b-a182-736b53e78e06"
+# Create the jump server
+resource "openstack_compute_instance_v2" "jumpmachine" {
+  name            = "jumpmachine"
+  flavor_name     = "c4-r8-d80"
+  image_id        = "f73ec00b-6ea9-456b-a182-736b53e78e06"
+  key_pair        = "${var.user_keyPiar}"
+  security_groups = ["default",openstack_compute_secgroup_v2.ssh.name,openstack_compute_secgroup_v2.http.name, openstack_compute_secgroup_v2.https.name, openstack_compute_secgroup_v2.tcp.name]
 
-  network {
-    uuid = openstack_networking_network_v2.network_1.id
-    port = openstack_networking_port_v2.port_1.id
-  }
-
-  metadata = {
-    role = "master"
-  }
+    network {
+        name = "network"
+        port = openstack_networking_port_v2.port_1.id
+    }
 
   availability_zone = "Education"
-  user_data = filebase64("${path.module}/userdata-master.tpl")
 }
 
-resource "openstack_compute_instance_v2" "instance_2" {
-  name            = "instance_2"
-  security_groups = [openstack_compute_secgroup_v2.secgroup_2.name]
-  key_pair        = "ps222vt_Keypair"
-
-  flavor_name = "c4-r16-d80"
-  image_id = "f73ec00b-6ea9-456b-a182-736b53e78e06"
-
-  network {
-    uuid = openstack_networking_network_v2.network_1.id
+resource "openstack_compute_instance_v2" "webserver" {
+  name            = "webserver"
+  flavor_name     = "c4-r8-d80"
+  image_id        = "f73ec00b-6ea9-456b-a182-736b53e78e06"
+  key_pair        = "${var.user_keyPiar}"
+  security_groups = ["default",openstack_compute_secgroup_v2.ssh.name,openstack_compute_secgroup_v2.http.name, openstack_compute_secgroup_v2.https.name, openstack_compute_secgroup_v2.tcp.name]
+network {
+    name = "network"
     port = openstack_networking_port_v2.port_2.id
-  }
-
-  metadata = {
-    role = "worker"
+    
   }
 
   availability_zone = "Education"
-  user_data = filebase64("${path.module}/userdata-worker.tpl")
 }
 
-resource "openstack_compute_instance_v2" "instance_3" {
-  name            = "instance_3"
-  security_groups = [openstack_compute_secgroup_v2.secgroup_3.name]
-  key_pair        = "ps222vt_Keypair"
-
-  flavor_name = "c4-r16-d80"
-  image_id = "f73ec00b-6ea9-456b-a182-736b53e78e06"
-
+# Create the web server
+resource "openstack_compute_instance_v2" "database" {
+  name            = "database"
+  flavor_name     = "c4-r8-d80"
+  image_id        = "f73ec00b-6ea9-456b-a182-736b53e78e06"
+  key_pair        = "${var.user_keyPiar}"
+  security_groups = ["default",openstack_compute_secgroup_v2.ssh.name,openstack_compute_secgroup_v2.http.name, openstack_compute_secgroup_v2.https.name, openstack_compute_secgroup_v2.tcp.name]
   network {
-    uuid = openstack_networking_network_v2.network_1.id
+    name = "network"
     port = openstack_networking_port_v2.port_3.id
   }
 
-  metadata = {
-    role = "worker"
-  }
 
   availability_zone = "Education"
-  user_data = filebase64("${path.module}/userdata-worker.tpl")
-}
-
-
-resource "openstack_blockstorage_volume_v3" "volume_1" {
-  name = "volume_1"
-  size = 20
-}
-
-resource "openstack_compute_volume_attach_v2" "attach_1" {
-  instance_id = openstack_compute_instance_v2.instance_1.id
-  volume_id   = openstack_blockstorage_volume_v3.volume_1.id
-}
-
-resource "openstack_blockstorage_volume_v3" "volume_2" {
-  name = "volume_2"
-  size = 10
-}
-
-resource "openstack_compute_volume_attach_v2" "attach_2" {
-  instance_id = openstack_compute_instance_v2.instance_2.id
-  volume_id   = openstack_blockstorage_volume_v3.volume_2.id
-}
-
-resource "openstack_blockstorage_volume_v3" "volume_3" {
-  name = "volume_3"
-  size = 10
-}
-
-resource "openstack_compute_volume_attach_v2" "attach_3" {
-  instance_id = openstack_compute_instance_v2.instance_3.id
-  volume_id   = openstack_blockstorage_volume_v3.volume_3.id
 }
 
 resource "openstack_networking_floatingip_v2" "floatingip_1" {
-  pool = var.PUBLIC_NETWORK_NAME # Replace with the name of the "public" network
-}
-
-resource "openstack_networking_floatingip_v2" "floatingip_2" {
-  pool = var.PUBLIC_NETWORK_NAME # Replace with the name of the "public" network
-}
-
-resource "openstack_networking_floatingip_v2" "floatingip_3" {
-  pool = var.PUBLIC_NETWORK_NAME # Replace with the name of the "public" network
+  pool = "public" # Replace with the name of the "public" network
 }
 
 resource "openstack_compute_floatingip_associate_v2" "associate_floatingip_1" {
   floating_ip = openstack_networking_floatingip_v2.floatingip_1.address
-  instance_id = openstack_compute_instance_v2.instance_1.id
-}
-
-resource "openstack_compute_floatingip_associate_v2" "associate_floatingip_2" {
-  floating_ip = openstack_networking_floatingip_v2.floatingip_2.address
-  instance_id = openstack_compute_instance_v2.instance_2.id
-}
-
-resource "openstack_compute_floatingip_associate_v2" "associate_floatingip_3" {
-  floating_ip = openstack_networking_floatingip_v2.floatingip_3.address
-  instance_id = openstack_compute_instance_v2.instance_3.id
+  instance_id = openstack_compute_instance_v2.jumpmachine.id
 }
